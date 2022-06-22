45285f8559bc8a9071f18061d173e1433f38bfa1
Ilya Zverkov <izverk78@yandex.ru>
2022-06-22 17:29:16 +0300

    fix: исправлены импорты в стилях, обновлен normalize, оптимизированы имена css классов, добавлены первые файлы скриптов

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,51 @@
-import daysListContainerSelector from '../utils/constants.js';
+import {
+	daysListContainerSelector,
+	dayTemplateSelector,
+	dayElementSelector,
+} from '../utils/constants.js';
 
 // ==================== ВЫБОР ЭЛМЕНТОВ DOM =====================
 
 // Контейнер сетки календаря
 const daysListContainer = document.querySelector(daysListContainerSelector);
 
+// Элемент шаблона карточки дня
+const dayTemplateElement = document.querySelector(dayTemplateSelector);
+
 // ==================== ОСНОВНОЙ АЛГОРИТМ =====================
+
+// Функция получения шаблона разметки для карточки дня
+const getDayElement = (dayTemplateElement, dayElementSelector) => {
+	const dayElement = dayTemplateElement.content
+		.querySelector(dayElementSelector)
+		.cloneNode(true);
+	console.log(
+		'🚀 ~ file: index.js ~ line 25 ~ getDayElement ~ dayElement',
+		dayElement
+	);
+
+	return dayElement;
+};
+
+// Функция рендеринга сетки календарных дней
+const daysRender = (container, dayTemplateElement, dayElementSelector) => {
+	const elementsArray = [...Array(35)].map((item, index) => {
+		const dayElement = getDayElement(dayTemplateElement, dayElementSelector);
+		dayElement.key = index;
+		return dayElement;
+	});
+
+	console.log(
+		'🚀 ~ file: index.js ~ line 36 ~ elementsArray ~ elementsArray',
+		elementsArray
+	);
+	elementsArray.forEach((item) => {
+		container.append(item);
+	});
+};
+
+// // Получаем елемент "календарный день"
+// const dayElement = getDayElement();
+
+// Рендерим сетку календарных дней
+daysRender(daysListContainer, dayTemplateElement, dayElementSelector);
