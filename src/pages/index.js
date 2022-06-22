db40d2b5d91e4a34d9f0c8baa361494932b28f13
Ilya Zverkov <izverk78@yandex.ru>
2022-06-22 14:00:37 +0300

    refactor: стандартизована файловая структура, оптимизированы имена css классов для карточки дня

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import daysListContainerSelector from '../utils/constants.js';
+
+// ==================== ВЫБОР ЭЛМЕНТОВ DOM =====================
+
+// Контейнер сетки календаря
+const daysListContainer = document.querySelector(daysListContainerSelector);
+
+// ==================== ОСНОВНОЙ АЛГОРИТМ =====================
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+// Селектор контейнера, в который динамически вставляется сетка дней (чисел) календаря
+export const daysListContainerSelector = '.calendar-grid__days-list';
